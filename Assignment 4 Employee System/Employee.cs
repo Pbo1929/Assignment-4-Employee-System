8f8b825d4c9dd097d12930c324108afb962b3648
Prem Worapon Sumsai <lulztit1929@gmail.com>
2021-01-18 12:06:56 +0700

    Add project files.

---
+++ Assignment 4 Employee System/Employee.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_4_Employee_System
+{
+    class Employee
+    {
+        private string Name;
+        private int Age;
+        private DateTime Dob;
+        private string Job;
+        private double Salary;
+
+        public static int EmployeeCount;
+        private static int RetirementAge = 65;
+
+        public Employee(string _Name, int _Age, DateTime _Dob, string _Job, double _Salary)
+        {
+            this.Name = _Name;
+            this.Age = _Age;
+            this.Salary = _Salary;
+            this.Job = _Job;
+            Dob = _Dob;
+
+            //EmployeeCount = EmployeeCount + 1
+            //EmployeeCount += 1;
+            EmployeeCount++; // These are all the same!
+
+        }
+
+        public double GetMonthlySalary()
+        {
+            double MonthlySalary = Salary / 12;
+            return MonthlySalary;
+        }
+
+        public bool isBirthday()
+        {
+            int month = Dob.Month;
+            int day = Dob.Day;
+
+            if(month == DateTime.Now.Month && day == DateTime.Now.Day)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void SetAge()
+        {
+                               //2021 - 2004 = 17
+            Age = DateTime.Now.Year - Dob.Year;
+        }
+
+        public int GetYearsUntilRetirement()
+        {
+            SetAge();
+
+            // 65 - 17 = 48
+            int YearsUntilRetirement = RetirementAge - Age;
+            return YearsUntilRetirement;
+        }
+
+        public string GetInformation()
+        {
+            string information = Name + Age + Job + Salary;
+            return information;
+        }
+
+        //mutator methods
+        public void SetJob(string newJob)
+        {
+            Job = newJob;
+        }
+        public void SetSalary(double newSalary)
+        {
+            Salary = newSalary;
+        }
+
+        public void SetName(string newName)
+        {
+            Name = newName;
+        }
+    }
+}
---
+++ Assignment 4 Employee System/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Assignment_4_Employee_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Toy = new Employee("Toy", new DateTime(2004, 1, 18),"Student",13);
+            Toy.SetJob("Plumber");
+            Toy.SetSalary(6);
+            Toy.SetName("Fake Toy");
+            Console.WriteLine(Toy.GetMonthlySalary());
+            Console.WriteLine(Toy.isBirthday());
+            Console.WriteLine(Toy.GetYearsUntilRetirement());
+
+            Employee Ankit = new Employee("Ankit", new DateTime(2004, 10, 25), "Pilot", 1231);
+            Console.WriteLine(Employee.EmployeeCount);
+        }
+    }
+}
